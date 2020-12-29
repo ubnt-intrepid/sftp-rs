e09586fd20f0c4b6999138ef72db9c294e982c6a
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-12-30 04:00:41 +0900

    init request

--- examples/sketch.rs
+++ examples/sketch.rs
@@ -1,5 +1,14 @@
 use anyhow::{ensure, Context as _, Result};
-use std::net::TcpStream;
+use byteorder::{NetworkEndian, ReadBytesExt as _};
+use std::{
+    convert::TryFrom,
+    env::var,
+    ffi::OsString,
+    io::{self, prelude::*},
+    net::TcpStream,
+    os::unix::prelude::*,
+};
+use zerocopy::{AsBytes, U32};
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
@@ -26,6 +35,13 @@ fn main() -> Result<()> {
 
     // TODO: communicate SFTP
 
+    send_init_packet(&mut channel, 6).context("failed to send SSH_FXP_INIT request packet")?;
+    tracing::debug!("send SSH_FXP_INIT");
+
+    let resp = receive_init_packet(&mut channel)
+        .context("failed to receive SSH_FXP_VERSION response packet")?;
+    tracing::debug!("--> {:?}", resp);
+
     Ok(())
 }
 
@@ -47,6 +63,128 @@ fn authenticate(ssh2: &ssh2::Session) -> Result<()> {
             continue;
         }
     }
+
+    Ok(())
+}
+
+// ====
+
+const SFTP_PROTOCOL_VERSION: u32 = 6;
+
+#[derive(AsBytes)]
+#[allow(non_camel_case_types)]
+#[repr(u8)]
+enum PacketType {
+    SSH_FXP_INIT = 1,
+    SSH_FXP_VERSION = 2,
+}
+
+type RequestId = u32;
+
+#[derive(AsBytes)]
+#[repr(packed)]
+struct InitPacket {
+    length: U32<NetworkEndian>,
+    typ: PacketType,
+    version: U32<NetworkEndian>,
+}
+
+fn send_init_packet<W>(mut writer: W, version: u32) -> io::Result<()>
+where
+    W: io::Write,
+{
+    let packet = InitPacket {
+        length: U32::new(1 + 4), // type + version
+        typ: PacketType::SSH_FXP_INIT,
+        version: U32::new(version),
+    };
+    writer.write_all(packet.as_bytes())?;
+    writer.flush()?;
+    Ok(())
+}
+
+#[derive(Debug)]
+struct InitResponse {
+    typ: u8,
+    version: u32,
+    extensions: Vec<(OsString, OsString)>,
+}
+
+fn receive_init_packet<R>(mut reader: R) -> io::Result<InitResponse>
+where
+    R: io::Read,
+{
+    // at first,
+    let length = reader.read_u32::<NetworkEndian>()?;
+
+    // a
+    let mut reader = reader.take(length as u64);
+
+    let typ = reader.read_u8()?;
+    let version = reader.read_u32::<NetworkEndian>()?;
+
+    let mut extensions = vec![];
+    loop {
+        let name = match read_packet_string(&mut reader)? {
+            Some(name) => name,
+            None => break,
+        };
+        let value = read_packet_string(&mut reader)?.unwrap();
+        extensions.push((name, value));
+    }
+
+    Ok(InitResponse {
+        typ,
+        version,
+        extensions,
+    })
+}
+
+fn read_packet_string<R>(mut reader: R) -> io::Result<Option<OsString>>
+where
+    R: io::Read,
+{
+    let len = match reader.read_u32::<NetworkEndian>() {
+        Ok(n) => n,
+        Err(ref err) if err.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
+        Err(err) => return Err(err),
+    };
+    let mut buf = vec![0u8; len as usize];
+    reader.read_exact(&mut buf[..])?;
+    Ok(Some(OsString::from_vec(buf)))
+}
+
+#[derive(AsBytes)]
+#[repr(packed)]
+struct PacketHeader {
+    length: U32<NetworkEndian>,
+    typ: PacketType,
+    request_id: U32<NetworkEndian>,
+}
+
+fn send_packet<W, T>(
+    mut writer: W,
+    packet_type: PacketType,
+    request_id: RequestId,
+    data: T,
+) -> io::Result<()>
+where
+    W: io::Write,
+    T: AsBytes,
+{
+    let data = data.as_bytes();
+
+    let length = u32::try_from(1 + 4 + data.len()).expect("too large data");
+
+    let header = PacketHeader {
+        length: U32::new(length),
+        typ: packet_type,
+        request_id: U32::new(request_id),
+    };
+
+    writer.write_all(header.as_bytes())?;
+    writer.write_all(data)?;
+    writer.flush()?;
 
     Ok(())
 }
