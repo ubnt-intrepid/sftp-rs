411efc378c196b6cc5df4dea2c14e1d87e5221a1
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-12-31 14:45:42 +0900

    fix sketch

--- examples/sketch.rs
+++ examples/sketch.rs
@@ -1,18 +1,30 @@
 use anyhow::{ensure, Context as _, Result};
-use std::net::TcpStream;
+use std::net::{IpAddr, SocketAddr, TcpStream};
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let tcp = TcpStream::connect("127.0.0.1:22").context("failed to establish TCP connection")?;
+    let mut args = pico_args::Arguments::from_env();
+
+    let host = args
+        .opt_value_from_str(["-h", "--host"])?
+        .unwrap_or_else(|| IpAddr::from([127, 0, 0, 1]));
+    let port = args.opt_value_from_str(["-p", "--port"])?.unwrap_or(22u16);
+    let username = args.free_from_str()?.unwrap_or_else(whoami::username);
+
+    let addr = SocketAddr::from((host, port));
+    tracing::debug!(?addr);
+    tracing::debug!(?username);
 
     let mut ssh2 = ssh2::Session::new().context("failed to create ssh2 session")?;
-    ssh2.set_tcp_stream(tcp);
+
+    let stream = TcpStream::connect(&addr).context("failed to establish TCP connection")?;
+    ssh2.set_tcp_stream(stream);
 
     ssh2.handshake()
         .context("errored on performing SSH handshake")?;
 
-    authenticate(&ssh2).context("failed to authenticate SSH session")?;
+    authenticate(&ssh2, &username).context("failed to authenticate SSH session")?;
     ensure!(ssh2.authenticated(), "SSH session is not authenticated");
 
     let mut channel = ssh2
@@ -22,6 +34,7 @@ fn main() -> Result<()> {
     channel
         .subsystem("sftp")
         .context("SFTP subsystem is not supported")?;
+
     tracing::debug!("start SFTP");
 
     // TODO: communicate SFTP
@@ -112,7 +125,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn authenticate(ssh2: &ssh2::Session) -> Result<()> {
+fn authenticate(ssh2: &ssh2::Session, username: &str) -> Result<()> {
     let mut agent = ssh2.agent().context("failed to init SSH agent handle")?;
     agent.connect().context("failed to connect to SSH agent")?;
 
@@ -124,9 +137,8 @@ fn authenticate(ssh2: &ssh2::Session) -> Result<()> {
         .context("failed to get identities from SSH agent")?;
     ensure!(!identities.is_empty(), "public keys is empty");
 
-    let username = whoami::username();
     for identity in identities {
-        if let Err(..) = agent.userauth(&username, &identity) {
+        if let Err(..) = agent.userauth(username, &identity) {
             continue;
         }
     }
